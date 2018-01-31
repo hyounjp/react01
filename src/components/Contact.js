4985fec766faa3e16b20b2530d1420e4e465846d
hjjo <hjjo@fastsystem.co.kr>
2018-01-31 14:44:39 +0900

    검색기능구현

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,6 +6,7 @@ export default class Contact extends Component {
   constructor(props){
     super(props);
     this.state={
+      keyword : "",
       contactData: [{
         name:'Abet',
         phone:'010-0000-0001'
@@ -20,10 +21,27 @@ export default class Contact extends Component {
         phone:'010-0000-0004'
       }]
     };
+
+    this._handleChange=this._handleChange.bind(this);
+
+  }
+
+  _handleChange(e){
+    this.setState({
+      keyword:e.target.value
+    });
   }
 
   render(){
     const mapToComponents = (data) => {
+      data.sort((a,b)=> { return a.name > b.name; });
+      data=data.filter(
+        (contact)=>{
+          return contact.name.toLowerCase().indexOf(this.state.keyword.toLowerCase()) > -1;
+        }
+      );
+
+
       return data.map((contact,i)=>{
         return (<ContactInfo contact={contact} key={i}/>);
       });
@@ -32,6 +50,12 @@ export default class Contact extends Component {
     return (
       <div>
         <h1>Contacts</h1>
+        <input
+          type="keyword"
+          placeholder="Search"
+          value={this.state.keyword}
+          onChange={this._handleChange}
+        />
         <div>{mapToComponents(this.state.contactData)}</div>
       </div>
     );
